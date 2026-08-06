b8682501e11fb193359a9df729c72649ae591f7d
Duvan <duvanbe2@gmail.com>
2026-08-06 09:23:29 +0200

    v4 import workfloud

--- n8n/start.sh
+++ n8n/start.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 
-echo "Iniciando n8n..."
-
-# Arranca n8n en segundo plano
-n8n start &
-
-# Espera a que termine de crear la BD
-sleep 25
-
 echo "Importando workflows..."
 
-n8n import:workflow \
-  --separate \
-  --input=/home/node/workflows
+n8n import:workflow --separate --input=/home/node/workflows
 
-echo "Workflows importados"
+echo "Iniciando n8n..."
 
-wait
+exec n8n start
